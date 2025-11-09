be2849daaa10a561edcfc2300dd1bcb0bce45ece
yildizaydnn <yildizaydd@gmail.com>
2025-11-09 18:40:22 +0300

    feat: Implemented file upload and photo creation feature

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 
 const ejs = require("ejs");
 const path = require("path");
+const fs = require("fs");
 const Photo = require("./models/Photo");
 
 const app = express();
@@ -19,14 +21,18 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //routes
 app.get("/", async (req, res) => {
-  const photos = await Photo.find({});
-  res.render("index", {
-    photos: photos,
-  });
+  const photos = await Photo.find({}).sort("-dateCreated");
+  res.render("index", { photos });
 });
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", { photo });
+});
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
@@ -35,8 +41,26 @@ app.get("/add", (req, res) => {
 });
 
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  //console.log(req.files.image);
+  // await Photo.create(req.body);
+  //res.redirect("/");
+
+  const uploadDir = "public/uploads";
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadedImage.name,
+    });
+    res.redirect("/");
+  });
 });
 
 const port = 3000;
