8b0cc4d1cdb505075573887cf6c009e53f89b465
yildizaydnn <yildizaydd@gmail.com>
2025-11-09 15:22:23 +0300

    feat: Added MongoDB (Mongoose) connection

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const ejs = require("ejs");
 const path = require("path");
+const Photo = require("./models/Photo");
 
 const app = express();
-const port = 3000;
 
-const myLogger = (req, res, next) => {
-  console.log("middleware log");
-  next();
-};
-
-const myLogger2 = (req, res, next) => {
-  console.log("middleware log 2");
-  next();
-};
-
-app.use(express.static("public"));
-app.use(myLogger);
-app.use(myLogger2);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "template/index.html"));
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+//template engine
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos: photos,
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ models/Photo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create a schema
+const PhotoSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Photo = mongoose.model("photo", PhotoSchema);
+
+module.exports = Photo;
